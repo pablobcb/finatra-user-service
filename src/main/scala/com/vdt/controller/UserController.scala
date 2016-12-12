bfc9c33b7111313bbbb8381528fef15b990be498
Pablo Botelho <pablo.bcb@gmail.com>
2016-12-12 20:14:24 -0200

    get user by id service

--- src/main/scala/com/vdt/controller/UserController.scala
+++ src/main/scala/com/vdt/controller/UserController.scala
@@ -2,11 +2,18 @@ package com.vdt.controller
 
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import org.joda.time.DateTime
 
 case class CreateUserRequest(email: String, password: String)
-case class CreateUserResponse(iAAAd: Int)
+case class CreateUserResponse(id: Int)
+
+case class FindUserResponse(id:Int, email: String, password: String, dateCreated: DateTime)
+
 
 class UserController extends Controller {
+
+  get("/user/:id") { request: Request =>
+  }
 
   post("/user") { request: CreateUserRequest =>
     response
